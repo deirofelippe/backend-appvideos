4bb5d2e6a8d89513ed38b415d87f3e7f1482a13e
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-26 11:51:48 -0300

    update app e server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const app = require("./app");
+const db = require("./database");
+
+const PORT = process.env.PORT || 3000;
+
+(async () => {
+   try {
+      await db.sync();
+      console.log("Database sync...");
+
+      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+   } catch (error) {
+      console.error(error);
+   }
+})();
