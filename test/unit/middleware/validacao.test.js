5821c47a1b0568286d803dfb0a17d3c0b329ec59
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-26 18:41:23 -0300

    test: validacao de usuario

---
+++ test/unit/middleware/validacao.test.js
@@ -0,0 +1,84 @@
+const {
+   describe,
+   test,
+   expect,
+   beforeEach,
+   beforeAll,
+} = require("@jest/globals");
+const faker = require("faker");
+const {
+   validarUsuario,
+} = require("../../../src/middleware/validacao/usuario.validacao.js");
+const logger = require("../../../src/logger.js");
+
+beforeAll(() => jest.spyOn(logger, "error").mockImplementation());
+
+describe("Validação de usuário", () => {
+   describe("#validarUsuario", () => {
+      const req = {
+         body: {},
+      };
+
+      const res = {
+         status: jest.fn().mockReturnValue({ send: jest.fn() }),
+      };
+
+      const next = jest.fn();
+
+      const expectedError = {
+         errors: {},
+      };
+
+      beforeEach(() => {
+         req.body = {};
+         expectedError.errors = {};
+      });
+
+      test("Email e nome deve ser preenchido", async () => {
+         req.body.nome = "";
+         req.body.email = "";
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.nome = [
+            "Deve ter no minimo 3 caracteres",
+            "Campo deve ser preenchido",
+         ];
+         expectedError.errors.email = ["Campo deve ser preenchido"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+      test("Nome deve ter um minimo de caracteres", async () => {
+         req.body.nome = "aa";
+         req.body.email = faker.internet.email();
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.nome = ["Deve ter no minimo 3 caracteres"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("Nome deve ter uma máximo de caracteres", async () => {
+         req.body.nome = "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+         req.body.email = faker.internet.email();
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.nome = ["Deve ter no maximo 50 caracteres"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("Email deve ser válido", async () => {
+         req.body.nome = faker.name.findName();
+         req.body.email = "abc";
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.email = ["Email inválido"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+   });
+});
