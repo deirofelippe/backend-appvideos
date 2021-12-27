494a8583d89b4382c06cc38801d003f98b9435b0
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-12-26 21:07:25 -0300

    test: rota do usuario com validacao

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const usuarioController = require("../controller/usuario.controller.js");
+const validarUsuario = require("../validacao/usuario.validacao.js");
 
 const router = express.Router();
 
-router.post("/usuario", usuarioController.create);
+router.post("/usuario", validarUsuario, usuarioController.create);
 router.get("/usuario", usuarioController.findAll);
 
 module.exports = router;
