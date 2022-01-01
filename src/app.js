1236678fb385b86c1827e4ecf074b835f626a64d
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-01 16:59:36 -0300

    app.js: adiciona helmet, cors e cookieParser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
