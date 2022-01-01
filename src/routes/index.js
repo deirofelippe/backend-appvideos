10d344d49c5a5b8c55499389869d8281bbf1cf2d
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-01 16:54:42 -0300

    base de rotas pra autenticacao com jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const usuarioController = require("../controller/usuario.controller.js");
+const loginController = require("../controller/login.controller.js");
 const validarUsuario = require("../validacao/usuario.validacao.js");
 
 const router = express.Router();
 
 router.post("/usuario", validarUsuario, usuarioController.create);
 router.get("/usuario", usuarioController.findAll);
+
+router.post("/login", loginController.login);
+router.post("/logout", loginController.logout);
 
 module.exports = router;
