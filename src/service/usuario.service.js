30166b128b695f6b565a9e00e6b4d829e8436101
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-01 17:02:44 -0300

    service usuario: adiciona cache e tratamento de erro pro create e findAll

--- src/service/usuario.service.js
+++ src/service/usuario.service.js
@@ -1,13 +1,33 @@
 const dao = require("../dao/usuario.dao.js");
 const uuid = require("uuid");
+const bcrypt = require("bcrypt");
+const cache = require("../cache");
 
-function findAll() {
-   return dao.findAll();
+async function findAll() {
+   const usuariosCache = await cache.buscarDadosNaCache("usuarios");
+   if (usuariosCache) return usuariosCache;
+
+   const usuarios = await dao.findAll();
+   if (!usuarios) return usuarios;
+
+   await cache.gravarDadosNaCache("usuarios", usuarios);
+   return usuarios;
 }
 
 async function create(usuario) {
+   const existeEmail = dao.findByEmail(usuario.email);
+   if (existeEmail)
+      throw { status: 401, errors: { email: ["Email já existente"] } };
+
    usuario.id = uuid.v4();
-   return await dao.create(usuario);
+
+   const salt = parseInt(process.env.BCRYPT_SALT);
+   const senhaHash = await bcrypt.hash(usuario.senha, salt);
+   usuario.senha = senhaHash;
+
+   const { senha, ...rest } = await dao.create(usuario);
+   await cache.removerDadosNaCache("usuarios");
+   return rest;
 }
 
 module.exports = {
