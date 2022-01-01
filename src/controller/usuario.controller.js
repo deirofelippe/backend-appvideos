6f1aae5dd2296d1a118c1a4e464298f49a43d07f
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-01 17:00:41 -0300

    controller usuario: tratamento de erro pro create e findAll

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1,13 +1,24 @@
+const logger = require("../logger.js");
 const service = require("../service/usuario.service.js");
 
 async function findAll(req, res) {
-   const result = await service.findAll();
-   res.json(result);
+   try {
+      const result = await service.findAll();
+      res.status(200).json(result);
+   } catch (error) {
+      const { status, errors } = error;
+      res.status(status).json(errors);
+   }
 }
 
 async function create(req, res) {
-   const result = await service.create(req.body);
-   res.json(result);
+   try {
+      const result = await service.create(req.body);
+      res.status(200).json(result);
+   } catch (error) {
+      const { status, errors } = error;
+      res.status(status).json(errors);
+   }
 }
 
 module.exports = {
