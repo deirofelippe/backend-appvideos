c2580f487631ecf6f7ab48195038f00c219e794c
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-01 17:01:27 -0300

    dao usuario: updates e tratamento de erro

--- src/dao/usuario.dao.js
+++ src/dao/usuario.dao.js
@@ -1,49 +1,68 @@
 const model = require("../models/usuario.js");
 const logger = require("../logger.js");
 
+const formatarRetorno = (result) => result?.dataValues;
+
 async function findAll() {
    try {
       const results = await model.findAll();
-      const usuarios = results.map((result) => result.dataValues);
+      const usuarios = results.map(formatarRetorno);
 
-      logger.info("Usuarios buscados!");
+      logger.info("Usuarios buscados: " + JSON.stringify(result));
       return usuarios;
    } catch (error) {
       logger.error(error);
-      throw Error(error);
+      return null;
    }
 }
 
 async function create(usuario) {
    try {
-      const { dataValues: result } = await model.create(usuario);
-      logger.info("Usuario adicionado!");
-      return result;
+      const result = await model.create(usuario);
+
+      logger.info("Usuario adicionado: " + JSON.stringify(result));
+      return result.dataValues;
    } catch (error) {
       logger.error(error);
-      throw Error(error);
+      return null;
+   }
+}
+
+async function findByEmail(email) {
+   try {
+      const result = await model.findOne({ where: email });
+
+      logger.info("Usuario buscado pelo email: " + JSON.stringify(result));
+      return result?.dataValues;
+   } catch (error) {
+      logger.error(error);
+      return null;
    }
 }
 
 async function findById(id) {
    try {
-      const { dataValues: result } = await model.findByPk(id);
-      logger.info("Usuario buscado!");
-      return result;
+      const result = await model.findByPk(id);
+
+      logger.info("Usuario buscado: " + JSON.stringify(result));
+      return result?.dataValues;
    } catch (error) {
       logger.error(error);
-      throw Error(error);
+      return null;
    }
 }
 
 async function findToUpdate(id) {
    try {
       const result = await model.findByPk(id);
-      logger.info("Usuario buscado para ser atualizado!");
+
+      logger.info(
+         "Usuario buscado para ser atualizado: " + JSON.stringify(result)
+      );
       return result;
    } catch (error) {
       logger.error(error);
-      throw Error(error);
+      return null;
    }
 }
 
@@ -52,22 +71,24 @@ async function update(novoUsuario) {
       const usuario = await findToUpdate(novoUsuario.id);
       usuario.dataValues = { ...usuario.dataValues, ...novoUsuario };
       const result = await usuario.save();
-      logger.info("Usuario atualizado!");
+
+      logger.info("Usuario atualizado: " + JSON.stringify(result));
       return result;
    } catch (error) {
       logger.error(error);
-      throw Error(error);
+      return null;
    }
 }
 
 async function remove(id) {
    try {
       const result = await model.destroy({ where: { id } });
-      logger.info("Usuario removido!");
+
+      logger.info("Usuario removido: " + JSON.stringify(result));
       return result;
    } catch (error) {
       logger.error(error);
-      throw Error(error);
+      return null;
    }
 }
 
@@ -75,6 +96,7 @@ module.exports = {
    remove,
    update,
    findById,
+   findByEmail,
    findAll,
    create,
 };
