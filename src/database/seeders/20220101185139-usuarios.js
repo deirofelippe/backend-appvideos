76c1cf42b2dd9ac37c5350b0cf9279945b1e8e9c
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-01 16:55:24 -0300

    seeders pra usuario

---
+++ src/database/seeders/20220101185139-usuarios.js
@@ -0,0 +1,39 @@
+"use strict";
+
+require("dotenv").config();
+const faker = require("faker");
+const uuid = require("uuid");
+const { cpf } = require("cpf-cnpj-validator");
+const bcrypt = require("bcrypt");
+
+module.exports = {
+   up: async (queryInterface, Sequelize) => {
+      const quantidade = 40;
+      const salt = parseInt(process.env.BCRYPT_SALT);
+
+      const usuarios = [];
+      let usuario = {};
+      let senha = "";
+
+      for (let indice = 1; indice <= quantidade; indice++) {
+         senha = faker.internet.password(15);
+         usuario = {
+            id: uuid.v4(),
+            nome: faker.name.findName(),
+            email: faker.internet.email(),
+            senha: await bcrypt.hash(senha, salt),
+            cpf: cpf.generate(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+         };
+
+         usuarios.push(usuario);
+      }
+
+      await queryInterface.bulkInsert("Usuarios", usuarios, {});
+   },
+
+   down: async (queryInterface, Sequelize) => {
+      await queryInterface.bulkDelete("Usuarios", null, {});
+   },
+};
