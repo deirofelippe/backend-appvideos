fc55b3adc04ecb2a3868e8f5214d5eaf2a73475c
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-03 15:52:26 -0300

    usuario.controller.create: destructuring no tratamento de erro

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -15,8 +15,7 @@ async function create(req, res) {
    try {
       const result = await service.create(req.body);
       res.status(200).json(result);
-   } catch (error) {
-      const { status, errors } = error;
+   } catch ({ status, errors }) {
       res.status(status).json(errors);
    }
 }
