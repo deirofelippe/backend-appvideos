42db65c7f8711fbd30ea7b30351bf2cde148b7a3
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-03 15:57:54 -0300

    atualiza usuario factory pra retornar somente array

--- test/usuarioFactory.js
+++ test/usuarioFactory.js
@@ -1,28 +1,22 @@
+const { cpf } = require("cpf-cnpj-validator");
 const faker = require("faker");
 const uuid = require("uuid");
-const { UUID } = require("sequelize/dist");
 faker.locale = "pt_BR";
 
 /**
  *
- * @param {number} quantidade por padrão o valor é 1
+ * @param {*} quantidade por padrão o valor é 1
  * @returns
  */
 function factory(quantidade = 1) {
-   if (quantidade === 1) {
-      return {
-         id: uuid.v4(),
-         nome: faker.name.findName(),
-         email: faker.internet.email(),
-      };
-   }
-
    const usuarios = [];
-   for (let index = 1; index < quantidade; index++) {
+   for (let index = 1; index <= quantidade; index++) {
       usuarios.push({
          id: uuid.v4(),
          nome: faker.name.findName(),
          email: faker.internet.email(),
+         cpf: cpf.generate(),
+         senha: faker.internet.password(15),
       });
    }
 
