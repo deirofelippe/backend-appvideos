89e3b89ef764c6e75b9273f0de0189875f27bde5
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-03 23:04:03 -0300

    trocando console.log pelo pino e mudando variavel de ambiente

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 const app = require("./app");
 const db = require("./database");
+const logger = require("./logger");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.API_PORT || 3000;
 
 (async () => {
    try {
       await db.sync();
-      console.log("Database sync...");
+      logger.info("Database sync...");
 
       app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
    } catch (error) {
-      console.error(error);
+      logger.error(error);
       await db.close();
    }
 })();
