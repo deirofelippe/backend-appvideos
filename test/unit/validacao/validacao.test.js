2e4f643189e4575fbf644806dc46575c15473f3f
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-03 22:12:48 -0300

    test.unit.validacao.usuario: adiciona mais testes

--- test/unit/validacao/validacao.test.js
+++ test/unit/validacao/validacao.test.js
@@ -2,12 +2,13 @@ const {
    describe,
    test,
    expect,
-   beforeEach,
    beforeAll,
+   afterEach,
 } = require("@jest/globals");
 const faker = require("faker");
 const validarUsuario = require("../../../src/validacao/usuario.validacao.js");
 const logger = require("../../../src/logger.js");
+const usuarioFactory = require("../../usuarioFactory.js");
 
 beforeAll(() => jest.spyOn(logger, "error").mockImplementation());
 
@@ -18,7 +19,8 @@ describe("Validação de usuário", () => {
       };
 
       const res = {
-         status: jest.fn().mockReturnValue({ send: jest.fn() }),
+         status: jest.fn().mockReturnThis(),
+         send: jest.fn(),
       };
 
       const next = jest.fn();
@@ -27,14 +29,18 @@ describe("Validação de usuário", () => {
          errors: {},
       };
 
-      beforeEach(() => {
+      afterEach(() => {
          req.body = {};
          expectedError.errors = {};
       });
 
-      test("Email e nome deve ser preenchido", async () => {
-         req.body.nome = "";
-         req.body.email = "";
+      test("Campos devem ser preenchidos: email, nome, senha e cpf", async () => {
+         req.body = {
+            nome: "",
+            email: "",
+            senha: "",
+            cpf: "",
+         };
 
          await validarUsuario(req, res, next);
 
@@ -43,12 +49,55 @@ describe("Validação de usuário", () => {
             "Campo deve ser preenchido",
          ];
          expectedError.errors.email = ["Campo deve ser preenchido"];
+         expectedError.errors.senha = [
+            "Deve ter no minimo 8 caracteres",
+            "Campo deve ser preenchido",
+         ];
+         expectedError.errors.cpf = [
+            "CPF inválido",
+            "Está no formato inválido",
+            "Campo deve ser preenchido",
+         ];
 
          expect(res.status().send).toHaveBeenCalledWith(expectedError);
       });
-      test("Nome deve ter um minimo de caracteres", async () => {
-         req.body.nome = "aa";
-         req.body.email = faker.internet.email();
+
+      test("Campos são validos", async () => {
+         const { id, ...usuario } = usuarioFactory()[0];
+
+         req.body = {
+            ...usuario,
+         };
+
+         await validarUsuario(req, res, next);
+
+         expect(res.status).toHaveBeenCalledTimes(0);
+         expect(next).toHaveBeenCalledTimes(1);
+      });
+
+      test("Nome deve ser preenchido", async () => {
+         const { id, ...usuario } = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            nome: "",
+         };
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.nome = [
+            "Deve ter no minimo 3 caracteres",
+            "Campo deve ser preenchido",
+         ];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("Nome deve ter quantidade minima de caracteres", async () => {
+         const usuario = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            nome: "aa",
+         };
 
          await validarUsuario(req, res, next);
 
@@ -57,20 +106,42 @@ describe("Validação de usuário", () => {
          expect(res.status().send).toHaveBeenCalledWith(expectedError);
       });
 
-      test("Nome deve ter uma máximo de caracteres", async () => {
-         req.body.nome = faker.lorem.words(40);
-         req.body.email = faker.internet.email();
+      test("Nome deve ter quantidade maxima de caracteres", async () => {
+         const nome = faker.lorem.words(40);
+
+         const usuario = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            nome,
+         };
 
          await validarUsuario(req, res, next);
 
-         expectedError.errors.nome = ["Deve ter no maximo 50 caracteres"];
+         expectedError.errors.nome = ["Deve ter no maximo 60 caracteres"];
 
          expect(res.status().send).toHaveBeenCalledWith(expectedError);
       });
 
-      test("Email deve ser válido", async () => {
-         req.body.nome = faker.name.findName();
-         req.body.email = "abc";
+      test("Email deve ser preenchido", async () => {
+         const { id, ...usuario } = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            email: "",
+         };
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.email = ["Campo deve ser preenchido"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("Email é inválido", async () => {
+         const { id, ...usuario } = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            email: "abc.com",
+         };
 
          await validarUsuario(req, res, next);
 
@@ -79,14 +150,113 @@ describe("Validação de usuário", () => {
          expect(res.status().send).toHaveBeenCalledWith(expectedError);
       });
 
-      test("Nao deve ter erro e deve chamar o metodo next()", async () => {
-         req.body.nome = faker.name.findName();
-         req.body.email = faker.internet.email();
+      test("Senha deve ser preenchida", async () => {
+         const { id, ...usuario } = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            senha: "",
+         };
 
          await validarUsuario(req, res, next);
 
-         expect(res.status).toHaveBeenCalledTimes(0);
+         expectedError.errors.senha = [
+            "Deve ter no minimo 8 caracteres",
+            "Campo deve ser preenchido",
+         ];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("Senha deve ter um minimo de caracteres", async () => {
+         const { id, ...usuario } = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            senha: "abcde",
+         };
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.senha = ["Deve ter no minimo 8 caracteres"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("CPF deve ser preenchido", async () => {
+         const usuario = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+            cpf: "",
+         };
+
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.cpf = [
+            "CPF inválido",
+            "Está no formato inválido",
+            "Campo deve ser preenchido",
+         ];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("CPF é válido nos formatos com e sem caracteres especiais", async () => {
+         const usuario = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+         };
+
+         req.body.cpf = "31286578078";
+         await validarUsuario(req, res, next);
+
          expect(next).toHaveBeenCalledTimes(1);
+
+         req.body.cpf = "312.865.780-78";
+         await validarUsuario(req, res, next);
+
+         expect(next).toHaveBeenCalledTimes(2);
+      });
+
+      test("CPF é inválido", async () => {
+         const usuario = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+         };
+
+         req.body.cpf = "31286555078";
+         await validarUsuario(req, res, next);
+
+         expectedError.errors.cpf = ["CPF inválido"];
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+      });
+
+      test("CPF está no formato inválido", async () => {
+         const usuario = usuarioFactory()[0];
+         req.body = {
+            ...usuario,
+         };
+
+         expectedError.errors.cpf = ["Está no formato inválido"];
+
+         req.body.cpf = "31286578078";
+         await validarUsuario(req, res, next);
+
+         expect(next).toHaveBeenCalledTimes(1);
+
+         req.body.cpf = "312.865.780.78";
+         await validarUsuario(req, res, next);
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+
+         req.body.cpf = "312-865-780-78";
+         await validarUsuario(req, res, next);
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
+
+         req.body.cpf = "312/865-780-78";
+         await validarUsuario(req, res, next);
+
+         expect(res.status().send).toHaveBeenCalledWith(expectedError);
       });
    });
 });
