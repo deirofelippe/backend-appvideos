8dba40153d957915657d0f97399f7f502698ce4b
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-05 01:48:21 -0300

    usuario.validacao: tira formatacao do cpf (req.body)

--- src/validacao/usuario.validacao.js
+++ src/validacao/usuario.validacao.js
@@ -1,11 +1,16 @@
 const logger = require("../logger.js");
 const schema = require("./usuario.schema.js");
+const tirarFormatacaoDoCPF = require("./utils.js");
 
 async function validarUsuario(req, res, next) {
    const usuario = req.body;
+   const cpf = usuario.cpf;
 
    try {
       await schema.validate(usuario, { abortEarly: false });
+
+      req.body.cpf = tirarFormatacaoDoCPF(cpf);
+
       next();
    } catch (errors) {
       logger.error(errors);
