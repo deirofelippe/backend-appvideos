50422adce541b610bd533c0b208744ec5fb29441
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-05 03:38:55 -0300

    test.integration.dao.usuario: testes do findAll

--- test/integration/dao/usuario.dao.test.js
+++ test/integration/dao/usuario.dao.test.js
@@ -4,24 +4,35 @@ const {
    expect,
    afterAll,
    beforeAll,
+   afterEach,
 } = require("@jest/globals");
 const connection = require("../../../src/database");
 const dao = require("../../../src/dao/usuario.dao.js");
 const usuarioFactory = require("../../usuarioFactory.js");
 const logger = require("../../../src/logger.js");
+const model = require("../../../src/models/usuario.js");
 const montarError = require("../../../src/utils/montarError");
+const truncate = require("../../truncate.js");
 
 jest.spyOn(logger, "info").mockImplementation();
 jest.spyOn(logger, "error").mockImplementation();
 
 describe("usuario.dao", () => {
+   afterEach(async () => await truncate(connection.models));
+
    beforeAll(async () => await connection.sync());
 
    afterAll(async () => await connection.close());
 
+   const error = montarError(500, { msg: ["Algo deu errado!"] });
+
    test.todo("#findByEmail Deve encontrar o usuario com o email procurado");
    test.todo("#findByEmail Não deve encontrar o usuario com o email procurado");
    test.todo("#findByEmail Deve lancar um erro");
+
+   test.todo("#findByCPF Deve encontrar o usuario com o cpf procurado");
+   test.todo("#findByCPF Não deve encontrar o usuario com o cpf procurado");
+   test.todo("#findByCPF Deve lancar um erro");
 
    test.todo("#findById Deve encontrar o usuario com o id procurado");
    test.todo("#findById Não deve encontrar o usuario com o id procurado");
@@ -64,7 +75,7 @@ describe("usuario.dao", () => {
    });
 
    describe("#findAll", () => {
-      test.only("Deve retornar usuarios", async () => {
+      test("Deve retornar usuarios", async () => {
          const usuarios = usuarioFactory(3);
 
          const result1 = await dao.findAll();
@@ -80,8 +91,22 @@ describe("usuario.dao", () => {
          expect(result2).toHaveLength(3);
       });
 
-      test.todo("#findAll Não deve retornar usuarios");
-      test.todo("#findAll Deve lancar um erro");
+      test("Não deve retornar usuarios", async () => {
+         const result = await dao.findAll();
+         expect(result).toHaveLength(0);
+      });
+
+      test("Deve lancar um erro", async () => {
+         jest.spyOn(model, model.findAll.name).mockImplementation(() => {
+            throw "";
+         });
+
+         const expectedError = error;
+
+         const findAll = async () => await dao.findAll();
+
+         await expect(findAll).rejects.toEqual(expectedError);
+      });
    });
 
    describe("#findById", () => {
