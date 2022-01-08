f6078180436a67a084f254668613006400722c72
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-08 02:55:49 -0300

    test.unit.service.usuario.update

--- test/unit/service/usuario.service.test.js
+++ test/unit/service/usuario.service.test.js
@@ -4,6 +4,7 @@ const cache = require("../../../src/cache");
 const service = require("../../../src/service/usuario");
 const dao = require("../../../src/dao/usuario.dao.js");
 const montarError = require("../../../src/utils/montarError.js");
+const rewire = require("rewire");
 
 const uuid = require("uuid");
 jest.mock("uuid");
@@ -157,6 +158,87 @@ describe("usuario.service", () => {
          expect(result.createdAt).toBeDefined();
          expect(result.updatedAt).toBeDefined();
          expect(result.senha).toBeUndefined();
+      });
+   });
+
+   describe("#update", () => {
+      test("Deve lançar um erro (BD ou achou cpf) em verificarSePodeUsarCPF", async () => {
+         const usuario = usuarioFactory()[0];
+
+         const updateRewire = rewire("../../../src/service/usuario/update.js");
+
+         const error =
+            "error: cpf esta sendo usado por outro usuario pu erro no BD";
+
+         updateRewire.__set__("verificarSePodeUsarCPF", async () => {
+            throw error;
+         });
+
+         jest
+            .spyOn(service, service.update.name)
+            .mockImplementation(async (usuario) => await updateRewire(usuario));
+
+         const update = async () => await service.update(usuario);
+
+         await expect(update).rejects.toEqual(error);
+      });
+
+      test("Deve lançar um erro (BD ou achou email) em verificarSePodeUsarEmail", async () => {
+         const usuario = usuarioFactory()[0];
+
+         const updateRewire = rewire("../../../src/service/usuario/update.js");
+
+         updateRewire.__set__("verificarSePodeUsarCPF", async () => ({}));
+
+         const error =
+            "error: email esta sendo usado por outro usuario pu erro no BD";
+
+         updateRewire.__set__("verificarSePodeUsarEmail", async () => {
+            throw error;
+         });
+
+         jest
+            .spyOn(service, service.update.name)
+            .mockImplementation(async (usuario) => await updateRewire(usuario));
+
+         const update = async () => await service.update(usuario);
+
+         await expect(update).rejects.toEqual(error);
+      });
+
+      test("Deve lançar um erro ao atualizar funcionário", async () => {
+         const usuario = usuarioFactory()[0];
+         const id = usuario.id;
+
+         jest.spyOn(dao, dao.findByCPF.name).mockImplementation(() => ({ id }));
+         jest
+            .spyOn(dao, dao.findByEmail.name)
+            .mockImplementation(() => ({ id }));
+
+         const error = "erro: atualizar funcionario";
+         jest.spyOn(dao, dao.update.name).mockImplementation(() => {
+            throw error;
+         });
+
+         const update = async () => await service.update(usuario);
+
+         await expect(update).rejects.toEqual(error);
+      });
+
+      test("Deve conseguir atualizar o usuario", async () => {
+         const usuario = usuarioFactory()[0];
+         const id = usuario.id;
+
+         jest.spyOn(dao, dao.findByCPF.name).mockImplementation(() => ({ id }));
+         jest
+            .spyOn(dao, dao.findByEmail.name)
+            .mockImplementation(() => ({ id }));
+
+         jest.spyOn(dao, dao.update.name).mockImplementation(() => usuario);
+
+         const result = await service.update(usuario);
+
+         expect(result).toEqual(usuario);
       });
    });
 });
