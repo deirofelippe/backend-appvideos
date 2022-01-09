370a8b74a03474817dc83f47c86eb5e4df759fea
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-09 20:44:05 -0300

    usuario.service: cria a funcao findById

---
+++ src/service/usuario/findById.js
@@ -0,0 +1,19 @@
+const dao = require("../../dao/usuario.dao.js");
+const cache = require("../../cache");
+
+async function findById(id) {
+   const temUsuarioNaCache = await cache.buscarDadosNaCache(`usuario:${id}`);
+   if (temUsuarioNaCache) return temUsuarioNaCache;
+
+   let usuario = await dao.findById(id);
+
+   if (!usuario) return usuario;
+
+   const { senha, createdAt, updatedAt, ...rest } = usuario;
+
+   usuario = rest;
+
+   await cache.gravarDadosNaCache(`usuario:${id}`, usuario);
+   return usuario;
+}
+module.exports = findById;
