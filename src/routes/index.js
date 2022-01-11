9a04654b3060d87e7fddb47141f58043d26de230
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-11 04:29:41 -0300

    adiciona rotas pros metodos delete, put e get de usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const usuarioController = require("../controller/usuario.controller.js");
 const loginController = require("../controller/login.controller.js");
-const validarUsuario = require("../validacao/usuario.validacao.js");
+const validacao = require("../validacao/usuario");
 
 const router = express.Router();
 
-router.post("/usuario", validarUsuario, usuarioController.create);
+router.post("/usuario", validacao.create, usuarioController.create);
 router.get("/usuario", usuarioController.findAll);
+router.get("/usuario/:id", usuarioController.findById);
+router.put("/usuario/:id", usuarioController.update);
+router.delete("/usuario/:id", usuarioController.remove);
 
 router.post("/login", loginController.login);
 router.post("/logout", loginController.logout);
