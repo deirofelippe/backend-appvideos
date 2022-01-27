696e6d913516e927a1aa01caafd068ed05df7f2b
felippedesouza <sergiofelippe.deiro@gmail.com>
2022-01-26 21:05:06 -0300

    cache.redisInstance: tratamento de excecao dentro do getInstance

--- src/cache/redisInstance.js
+++ src/cache/redisInstance.js
@@ -1,12 +1,20 @@
+const logger = require("../logger");
+const Redis = require("ioredis");
+
 require("dotenv").config();
 
 function getInstance() {
-   const Redis = require("ioredis");
-   const port = parseInt(process.env.REDIS_PORT);
-   return new Redis({
-      port: port || 6379,
-      host: process.env.REDIS_HOST || "redis",
-   });
+   try {
+      const port = parseInt(process.env.REDIS_PORT);
+      return new Redis({
+         connectTimeout: 1000,
+         port: port || 6379,
+         host: process.env.REDIS_HOST || "redis",
+      });
+   } catch (error) {
+      logger.error("[ERRO DE CONEXAO REDIS]: " + error);
+      return null;
+   }
 }
 
 module.exports = getInstance;
